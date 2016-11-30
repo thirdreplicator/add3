5f0578ac70a838b11c2e1aec34f7a0e1a24f7b11
David Beckwith <thirdrepliator@gmail.com>
2016-11-30 10:47:08 +0800

    .gitignore + initial code commit.

---
+++ index.js
@@ -0,0 +1,3 @@
+var add3 = require('./src/add3')
+
+module.exports = add3
---
+++ src/add3.js
@@ -0,0 +1,7 @@
+var add2 = require('add2')
+
+function add3(a, b, c) {
+  return add2(a, b) + c;
+}
+
+module.exports = add3
